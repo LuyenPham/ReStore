7b9075bd5a76f2613ad9751f5e0ad65cd7ec61a0
DESKTOP-5CE87SS\Luyen Pham <Luyen_phamnhu@datahouse.com>
2022-08-21 18:06:29 +0700

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,51 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+Console.WriteLine("connection string:" + connectionString);
+
+builder.Services.AddDbContext<StoreContext>(opt => {
+    opt.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine("IsDevelopment:true");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    var context = scope.ServiceProvider.GetRequiredService<StoreContext>();    
+
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (System.Exception ex)
+{
+    logger.LogError(ex,"Problem migrating data");
+}
+
+app.Run();
